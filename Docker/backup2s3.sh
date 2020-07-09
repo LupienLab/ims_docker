9c079c471984ed2afc9036f5e48bc19f2ad2ed0a
ankita <ankita@10.0.0.188>
2020-07-09 10:07:13 -0400

    backup

---
+++ Docker/backup2s3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+##BACKUP the database
+FOLDER="/home/ubuntu/db_backups"
+now_local=`date +'%Y-%m-%d'`
+now_s3=`date +'%Y/%m/%d'`
+SQLFILE=$FOLDER/${now_local}.sql.txt
+docker exec -u postgres docker_db_1 pg_dump -F p ims_db > $SQLFILE
+##cp to amazon here
+/usr/local/bin/aws s3 cp $SQLFILE  s3://dekkerlab-web/db-backups/${now_s3}.sql.txt &> /home/ubuntu/aws_backup_result.txt
+#rm the file
+rm $SQLFILE
+#BACKUP the media files
+/usr/local/bin/aws s3 sync /home/ubuntu/media s3://dekkerlab-web/media-backups
