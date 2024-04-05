c8ec97c81eb661bb90a5ac3007f95177a4e85d3a
nandankita <nandankita@gmail.com>
2024-04-05 14:27:14 -0400

    Update backup2h4h.sh

--- Docker/backup2h4h.sh
+++ Docker/backup2h4h.sh
@@ -5,3 +5,7 @@ now_local=`date +'%Y-%m-%d'`
 now_s3=`date +'%Y/%m/%d'`
 SQLFILE=$FOLDER/${now_local}.lupien.ims.sql.txt
 docker exec -u postgres docker-db-1 pg_dump -F p ims_db > $SQLFILE
+
+######crontab -e ######
+#####
+##30 17 * * 5 bash /home/anand/duncan/ims_docker/Docker/backup2h4h.sh
