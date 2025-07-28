9113cc7478c0fd57eaaaac05f146f3ce9a327261
Wisam Al Abed <wisam.alabed@hey.com>
2025-07-28 11:00:02 -0400

    small fix to settings

--- ims/ims/settings.py
+++ ims/ims/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-BASE_URL = os.getenv("BASE_URL")
+BASE_URL = os.getenv("BASE_URL") or "http://localhost:8000"
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
